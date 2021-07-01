af0ea3cabb35f6dbcc5df0890c8a1321dd755b90
Sergey Volodin <whiteigel@gmail.com>
2021-07-01 21:20:25 +0200

    Line-by-line output done

--- main.py
+++ main.py
@@ -9,33 +9,35 @@ file2_path = os.path.join(os.getcwd(), file2)
 file3_path = os.path.join(os.getcwd(), file3)
 output_path = os.path.join(os.getcwd(), output_file)
 
-with open(file1_path, 'r', encoding='utf-8') as f1:
-    read1 = f1.readlines()
-with open(file2_path, 'r', encoding='utf-8') as f2:
-    read2 = f2.readlines()
-with open(file3_path, 'r', encoding='utf-8') as f3:
-    read3 = f3.readlines()
+read1 = open(file1_path, 'r', encoding='utf-8' )
+read1_list = []
+for line in read1:
+    read1_list.append(line)
+read2 = open(file2_path, 'r', encoding='utf-8' )
+read2_list = []
+for line in read2:
+    read2_list.append(line)
+read3 = open(file3_path, 'r', encoding='utf-8' )
+read3_list = []
+for line in read3:
+    read3_list.append(line)
 
-output_data1 = [file1_path + '\n', str(len(read1)), '\n'+ str(read1) + '\n']
-output_data2 = [file2_path + '\n', str(len(read2)), '\n'+ str(read2) + '\n']
-output_data3 = [file3_path + '\n', str(len(read3)), '\n'+ str(read3) + '\n']
+output_data1 = ['\n', file1_path +'\n', str(len(read1_list)) +'\n', (read1_list)]
+output_data2 = ['\n', file2_path +'\n', str(len(read2_list)) +'\n', (read2_list)]
+output_data3 = ['\n', file3_path +'\n', str(len(read3_list)) +'\n', (read3_list)]
 
-sort_len = len([output_data1, output_data2, output_data3])
-# print(sort_len)
-# sort_len = sorted(sort_len)
-# print(sort_len)
-output = []
 output_list = [output_data1, output_data2, output_data3]
-# print(output_list)
+output = []
 for ind, elm in enumerate(output_list):
     output.append(elm)
-    # output.sort(key = elm[1])
-# print(output)
+output.sort(key=len) # как отсортировать список по
 
-def write_output(output_list, output_path):
+def write_output(output_data, output_path):
     write_to_file = open(output_path, "w")
-    for elm in output_list:
-        write_to_file.writelines(elm)
+    for elm in output_data:
+        # print(elm)
+        for part in elm:
+            write_to_file.writelines(part)
     write_to_file.close()
 
-write_output(output_list, output_file)
+write_output(output, output_file)
