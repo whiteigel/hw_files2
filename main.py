80b3d60e940cad603a95723e6346b1c7e4a3d36f
Sergey Volodin <whiteigel@gmail.com>
2021-07-01 21:48:41 +0200

    Reading with "with"

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ file2_path = os.path.join(os.getcwd(), file2)
 file3_path = os.path.join(os.getcwd(), file3)
 output_path = os.path.join(os.getcwd(), output_file)
 
-read1 = open(file1_path, 'r', encoding='utf-8' )
-read1_list = []
-for line in read1:
-    read1_list.append(line)
-read2 = open(file2_path, 'r', encoding='utf-8' )
-read2_list = []
-for line in read2:
-    read2_list.append(line)
-read3 = open(file3_path, 'r', encoding='utf-8' )
-read3_list = []
-for line in read3:
-    read3_list.append(line)
+with open(file1_path, 'r', encoding='utf-8' ) as read:
+    read1_list = []
+    for line in read:
+        read1_list.append(line)
+with open(file2_path, 'r', encoding='utf-8' ) as read:
+    read2_list = []
+    for line in read:
+        read2_list.append(line)
+with open(file3_path, 'r', encoding='utf-8' ) as read:
+    read3_list = []
+    for line in read:
+        read3_list.append(line)
 
 output_data1 = ['\n', file1_path +'\n', str(len(read1_list)) +'\n', (read1_list)]
 output_data2 = ['\n', file2_path +'\n', str(len(read2_list)) +'\n', (read2_list)]
@@ -30,12 +30,11 @@ output_list = [output_data1, output_data2, output_data3]
 output = []
 for ind, elm in enumerate(output_list):
     output.append(elm)
-output.sort(key=len) # как отсортировать список по
+output.sort(key=len) # как отсортировать список по количеству срок в файле?
 
 def write_output(output_data, output_path):
     write_to_file = open(output_path, "w")
     for elm in output_data:
-        # print(elm)
         for part in elm:
             write_to_file.writelines(part)
     write_to_file.close()
