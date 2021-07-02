e0701534a2e2a8c08597f8302508e8ca3db5e0a3
Sergey Volodin <whiteigel@gmail.com>
2021-07-02 21:48:55 +0200

    Sorting func added

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ file2_path = os.path.join(os.getcwd(), file2)
 file3_path = os.path.join(os.getcwd(), file3)
 output_path = os.path.join(os.getcwd(), output_file)
 
+# paths = [file1_path, file2_path, file3_path]
+# for ind, elm in enumerate(paths):
+#     with open(elm, 'r', encoding='utf-8') as read:
+#         read_list = []
+#         for line in read:
+#            read_list.append(line)
+#         print(read_list)
+
+
 with open(file1_path, 'r', encoding='utf-8' ) as read:
     read1_list = []
     for line in read:
@@ -22,21 +31,17 @@ with open(file3_path, 'r', encoding='utf-8' ) as read:
     for line in read:
         read3_list.append(line)
 
-output_data1 = ['\n', file1_path +'\n', str(len(read1_list)) +'\n', (read1_list)]
-output_data2 = ['\n', file2_path +'\n', str(len(read2_list)) +'\n', (read2_list)]
-output_data3 = ['\n', file3_path +'\n', str(len(read3_list)) +'\n', (read3_list)]
+output_data1 = [file1_path +'\n', str(len(read1_list))+'\n', (read1_list), ['\n']]
+output_data2 = [file2_path +'\n', str(len(read2_list))+'\n', (read2_list), ['\n']]
+output_data3 = [file3_path +'\n', str(len(read3_list))+'\n', (read3_list), ['\n']]
 
 output_list = [output_data1, output_data2, output_data3]
-output = []
-for ind, elm in enumerate(output_list):
-    output.append(elm)
-output.sort(key=len) # как отсортировать список по количеству срок в файле?
+output_list.sort(key=lambda item: len(item[2]))
 
 def write_output(output_data, output_path):
-    write_to_file = open(output_path, "w")
-    for elm in output_data:
-        for part in elm:
-            write_to_file.writelines(part)
-    write_to_file.close()
+    with open(output_path, "w") as output:
+        for elm in output_data:
+            for part in elm:
+                output.writelines(part)
 
-write_output(output, output_file)
+write_output(output_list, output_file)
